5d8ee92e4fae15afd3f781c09cc069dc4b0f7f96
Clebson Moura <clebson.sh@gmail.com>
2022-05-26 08:28:48 -0300

    removido strict mode pra evitar rederização dupla

--- src/index.js
+++ src/index.js
@@ -3,8 +3,4 @@ import ReactDOM from "react-dom/client";
 import AppRoutes from "./AppRoutes";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <AppRoutes />
-  </React.StrictMode>
-);
+root.render(<AppRoutes />);
